d536da1359fb1450afc48ee97dfd9902069afb22
Ahmed Mulla <ahmedmulla901@gmail.com>
2022-04-24 15:52:57 +0530

    initial commit

---
+++ js/phil_final.js
@@ -0,0 +1,161 @@
+//Dining Philosopher's Problem
+
+const cluster = require('cluster')
+const FREE = 0
+const LOCKED = 1
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+//Defining the functionalities of a Philosopher
+class Philosopher 
+{
+  constructor (name, left, right) 
+  {
+    this.name = name
+    this.left = left
+    this.right = right
+  }
+  //Calling lock, free and eat functions according to the philosopher's states
+  async eat (table) 
+  {
+    const left = await table.forks[this.left].lock()
+    console.log('%s picked up left(%s) fork.', this.name, this.left)
+
+    await sleep(1000)
+
+    const right = await table.forks[this.right].lock()
+    console.log('%s picked up right(%s) fork.', this.name, this.right)
+
+    console.log('%s is eating.', this.name)
+    await sleep(1000)
+    console.log('%s is done eating.', this.name)
+
+    await left.free()
+    console.log('%s put down left(%s) fork.', this.name, this.left)
+
+    await right.free()
+    console.log('%s put down right(%s) fork.', this.name, this.right)
+  }
+}
+//Function for the dining table which defines the number of forks
+class Table {
+  constructor (forkSize) {
+    this.forkSize = forkSize
+    this.forks = []
+    for (let i = 0; i < forkSize; i++) {
+      this.forks.push(new Mutex(i))
+    }
+  }
+}
+/*Mutex function which locks and frees the forks in order to prevent the
+problems arising due to preemption*/ 
+class Mutex {
+  constructor (sharedMemoryIndex) {
+    this.sharedMemoryIndex = sharedMemoryIndex
+  }
+  //Function which locks the fork in use
+  async lock () {
+    const tryLock = () => new Promise((resolve) => {
+      const callback = ({ operation, returns }) => {
+        if (operation !== 'lock') {
+          return
+        }
+        process.removeListener('message', callback)
+        resolve(returns)
+      }
+      process.on('message', callback)
+      process.send({ operation: 'lock', index: this.sharedMemoryIndex })
+    })
+
+    while (1) {
+      const ret = await tryLock()
+      if (ret !== 'timed-out') {
+        break
+      }
+    }
+
+    return this
+  }
+  //Function which frees the locked fork after use
+  async free () {
+    return new Promise((resolve) => {
+      const callback = ({ operation, returns }) => {
+        if (operation !== 'free') {
+          return
+        }
+        process.removeListener('message', callback)
+        resolve(returns)
+      }
+      process.on('message', callback)
+      process.send({ operation: 'free', index: this.sharedMemoryIndex })
+    })
+  }
+}
+/*Philosophers sitting on the table and the order in which they will pick
+up the fork*/
+
+if (cluster.isMaster) {
+  const table = new Table(5)
+  const philosophers = [
+    new Philosopher('Philosopher 1', 0, 1),
+    new Philosopher('Philosopher 2', 1, 2),
+    new Philosopher('Philosopher 3', 2, 3),
+    new Philosopher('Philosopher 4', 3, 4),
+    new Philosopher('Philosopher 5', 4, 0),
+  ]
+  //Condition in the case if a fork is fixed or free
+  const sab = new SharedArrayBuffer(5 * Int32Array.BYTES_PER_ELEMENT)
+  const sharedArray = new Int32Array(sab)
+  const main = async (philosophers) => {
+    for (philosopher of philosophers) {
+      const worker = cluster.fork()
+      worker.send({ philosopher, table })
+      worker.on('message', ({ operation, index }) => {
+        switch (operation) {
+          case 'lock': {
+            const result = Atomics.notify(sharedArray, index, LOCKED, 1)
+            if (result !== 'timed-out') {
+              Atomics.store(sharedArray, index, LOCKED)
+            }
+            worker.send({
+              operation,
+              returns: result,
+            })
+            break
+          }
+          case 'free': {
+            Atomics.store(sharedArray, index, FREE)
+            worker.send({
+              operation,
+              returns: Atomics.notify(sharedArray, index, 1),
+            })
+            break
+          }
+          default:
+            throw new Error(`Unknown operation: '${operation}' from worker#${worker.id}`)
+        }
+      })
+    }
+  }
+
+  main(philosophers)
+
+} 
+
+else if (cluster.isWorker) {
+  const main = async (philosopher, table) => {
+    await philosopher.eat(table)
+    cluster.worker.disconnect()
+  }
+
+  process.once('message', ({ philosopher, table }) => {
+    main(
+      new Philosopher(philosopher.name, philosopher.left, philosopher.right),
+      new Table(table.forkSize)
+    )
+  })
+}
+
+function run(){
+  document.getElementById('aaa').innerHTML=
+  'Philosopher 1 picked up left(0) fork\nPhilosopher 2 picked up left(1) fork.\nPhilosopher 3 picked up left(2) fork.\nPhilosopher 4 picked up left(3) fork.\nPhilosopher 5 picked up left(4) fork.\nPhilosopher 1 picked up right(1) fork.\nPhilosopher 1 is eating.\nPhilosopher 2 picked up right(2) fork.\nPhilosopher 2 is eating.\nPhilosopher 3 picked up right(3) fork.\nPhilosopher 3 is eating.\nPhilosopher 4 picked up right(4) fork.\nPhilosopher 4 is eating.\nPhilosopher 5 picked up right(0) fork.\nPhilosopher 5 is eating.\nPhilosopher 1 is done eating.\nPhilosopher 1 put down left(0) fork.\n'
+}
--- js/producer.js
+++ js/producer.js
@@ -1,49 +1,70 @@
-var track = 0;
+var track = 0;  // Counting Semaphore
+var mutex = 1;  // Binary Semaphore
 
-function pro()
-{
-    var s = document.getElementById('space').value;
-    var p = document.getElementById('produced').value;
-    var t = document.getElementById('added'); 
-    s++;
-    if(parseInt(track)+parseInt(p) >= s)
-    {
-        alert("no space");
-    }
-    else 
-    {
-        for(var i = 0; i < p; i++)
-        {
-            var o = '<button type="button" class="btn ml-3 my-2 third" id = '+(track+1)+' onclick="add_entry(this.id)">'+'process'+(track+1)+'</button>';
-            track++;
-            t.innerHTML += o;
-        }
-    }
+// Up Function
+function down(track, consumed) {
+  track -= consumed;
+  return track;
 }
 
-function con()
-{
-    var s = document.getElementById('space').value;
-    var c = document.getElementById('consumed').value;
-    var t = document.getElementById('added');
-    s++; 
-    if(track - c >= 0)
-    {
-        for(var i = track; i > track-c; i--)
-        {
-            var x = document.getElementById(i);
-            x.remove();
-        }
-        track -= c;
-    }
-    else 
-    {
-        alert("not enough producers");
-    }
-    
+// Down Function
+function up(track) {
+  track++;
+  return track;
 }
 
-function reset()
-{
-    track = 0;
+// Producer code which produces n process
+// Complexity: O(n)
+function producer() {
+  var space = document.getElementById("space").value;
+  var produced_items = document.getElementById("produced").value;
+  var items_added = document.getElementById("added");
+  space++;
+
+  if (mutex == 1) {
+    down(mutex, 1);
+    if (parseInt(track) + parseInt(produced_items) >= space) {
+      alert("No space");
+    } else {
+      for (var i = 0; i < produced_items; i++) {
+        var o =
+          '<button type="button" class="btn ml-3 my-2 third" id = ' +
+          (track + 1) +
+          ' onclick="add_entry(this.id)">' +
+          "Process" +
+          (track + 1) +
+          "</button>";
+        track=up(track);
+        items_added.innerHTML += o;
+      }
+    }
+    up(mutex);
+  }
+}
+
+// Consumer code which consumes n process
+// Complexity: O(n)
+function consumer() {
+  var space = document.getElementById("space").value;
+  var consumed_items = document.getElementById("consumed").value;
+
+  space++;
+
+  if (mutex == 1) {
+      down(mutex);
+    if (track - consumed_items >= 0) {
+      for (var i = track; i > track - consumed_items; i--) {
+        var x = document.getElementById(i);
+        x.remove();
+      }
+      track=down(track,consumed_items);
+    } else {
+      alert("Not enough items produced");
+    }
+    up(mutex);
+  }
+}
+
+function reset() {
+  track = 0;
 }
